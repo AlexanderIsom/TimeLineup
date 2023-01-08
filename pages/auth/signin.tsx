8e5059dc96b41c72c49a866f53797b5230940193
Alexander Isom <alexanderdisom@gmail.com>
2023-01-08 00:57:06 +0000

    added discord login button to signin page

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,7 +1,5 @@
 import { signIn, getProviders } from "next-auth/react";
-import Image from "next/image";
 import Header from "../../components/Header";
-import backgroundImage from "../../public/RubberBandits.png";
 
 import AnimateSphereBackground from "../../components/AnimateSpheres";
 
@@ -10,7 +8,7 @@ function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function singin({ providers }: any) {
+export default function SignIn({ providers }: any) {
   return (
     <>
       <Header title="Login" />
@@ -33,6 +31,17 @@ export default function singin({ providers }: any) {
             </p>
 
             <div className={styles.btns}>
+              {providers &&
+                Object.values(providers).map((provider: any) => (
+                  <button
+                    key={provider.id}
+                    className={classNames(styles.btn, styles[provider.id])}
+                    onClick={() => signIn(provider.id)}
+                  >
+                    {provider.name.toUpperCase()}
+                  </button>
+                ))}
+
               <button className={classNames(styles.btn, styles.transparent)}>
                 Coming soon
               </button>
