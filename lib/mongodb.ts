dbaa33532c4cc54748fda839e18f6cac90922452
Alexander Isom <alexanderdisom@gmail.com>
2023-01-08 23:51:33 +0000

    fixed mongo db lib errors

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -10,14 +10,18 @@ const options = {}
 let client
 let clientPromise: Promise<MongoClient>
 
+let globalWithMongo = global as typeof globalThis & {
+  _mongoClientPromise: Promise<MongoClient>
+}
+
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
+  if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
+    globalWithMongo._mongoClientPromise = client.connect()
   }
-  clientPromise = global._mongoClientPromise
+  clientPromise = globalWithMongo._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
