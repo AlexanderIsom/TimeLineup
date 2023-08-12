8b5d1f861e153958eda2c23b841cf3b015f06979
Alexander Isom <37072462+AlexanderIsom@users.noreply.github.com>
2023-08-13 00:08:36 +0100

    Removed font references to Inter, added global nunito font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import "../styles/globals.css";
 import "../styles/Components/Resizable.css";
 import type { AppProps } from "next/app";
 import CustomNavbar from "../components/Navbar";
+import { Nunito } from "@next/font/google";
+
+const nunito = Nunito({ subsets: ['latin'] })
 
 export default function App({
   Component,
@@ -9,6 +12,11 @@ export default function App({
 }: AppProps) {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: ${nunito.style.fontFamily};
+        }
+      `}</style>
       <main style={{ display: "grid", gridTemplateRows: "64px 1fr" }}>
         <CustomNavbar />
         <Component {...pageProps} />
