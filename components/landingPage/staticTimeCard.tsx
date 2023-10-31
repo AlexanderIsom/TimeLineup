f6358bd46e776183ebf3533b1a39731471bc12f0
Alexander Isom <37072462+AlexanderIsom@users.noreply.github.com>
2023-10-31 00:05:51 +0000

    added grid

--- components/landingPage/staticTimeCard.tsx
+++ components/landingPage/staticTimeCard.tsx
@@ -2,6 +2,9 @@ import styles from "@/style/Components/TimeCard.module.scss"
 
 export default function StaticTimeCard(){
     return (<div className={styles.container} style={{width: "100%"}}>
-            <div className={`${styles.timeContainer}`}></div>
+            <div className={`${styles.timeContainer}`}>
+                <span className={styles.timeCue}>9:15</span>
+                <span className={styles.timeCue}>12:30</span>
+            </div>
         </div>)
 }
