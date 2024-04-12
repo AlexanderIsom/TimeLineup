526843e48e09fd853a3691ca314ab80737874722
Alexander Isom <alexanderdisom@gmail.com>
2024-04-12 23:22:35 +0100

    fix new event button is too small on loading state

--- src/app/events/loading.tsx
+++ src/app/events/loading.tsx
@@ -5,7 +5,7 @@ import { Card } from "@/components/ui/card";
 export default function Loading() {
 	return <div className="h-full">
 		<Card className="flex mx-4 mt-2 p-4 items-center align-middle">
-			<Button disabled={true}>New event</Button>
+			<Button size={"lg"} disabled={true}>New event</Button>
 		</Card>
 		<div className="flex gap-4 p-4 h-[90%]">
 			<EventCardSkeleton title={"Last 14 days"} description="Past events from the last 2 weeks." />
