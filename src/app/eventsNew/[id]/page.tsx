2fd5a2705127b33d7d289f64668e52cca42ca908
Alexander Isom <37072462+AlexanderIsom@users.noreply.github.com>
2024-05-05 23:30:35 +0100

    made resizable new cards work with scale

--- src/app/eventsNew/[id]/page.tsx
+++ src/app/eventsNew/[id]/page.tsx
@@ -1,24 +1,31 @@
 "use client"
 
-import React, { useEffect, useRef, useState } from "react";
-import { addHours, differenceInHours } from "date-fns";
+import React, { SyntheticEvent, useEffect, useRef, useState } from "react";
+import { addHours, differenceInMinutes, format, addMinutes } from "date-fns";
+import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
+import { Resizable, ResizeCallbackData } from "react-resizable";
 
-const START_TIME = new Date(); // Start time in hours
-const END_TIME = addHours(new Date(), 9); // End time in hours
-const TOTAL_HOURS = differenceInHours(END_TIME, START_TIME);
+const START_TIME = new Date(2024, 0, 1, 13, 0, 0); // Start time in hours
+const END_TIME = addHours(START_TIME, 9); // End time in hours
+const TOTAL_MINUTES = differenceInMinutes(END_TIME, START_TIME);
 const DIV_C_START_TIME = addHours(START_TIME, 2)
 const DIV_C_END_TIME = addHours(START_TIME, 5)
+
+const defaultWidth = 1000 / TOTAL_MINUTES;
 
 export default function ViewEvent({ params }: { params: { id: string } }) {
 	const divARef = useRef<HTMLDivElement>(null);
 	const divBRef = useRef<HTMLDivElement>(null);
-	const [hourWidth, setHourWidth] = useState(50);
+	const [minuteWidth, setMinuteWidth] = useState(defaultWidth);
+
+	const [startTime, setStartTime] = useState(DIV_C_START_TIME);
+	const [endTime, setEndTime] = useState(DIV_C_END_TIME);
 
 	useEffect(() => {
 		var divb = divBRef.current;
 		const resizeObserver = new ResizeObserver(entries => {
 			for (let entry of entries) {
-				setHourWidth(entry.contentRect.width / TOTAL_HOURS);
+				setMinuteWidth(entry.contentRect.width / TOTAL_MINUTES);
 			}
 		});
 
@@ -35,20 +42,42 @@ export default function ViewEvent({ params }: { params: { id: string } }) {
 
 	const handleDoubleClick = (e: React.MouseEvent) => {
 		const x = e.clientX - divBRef.current!.getBoundingClientRect().left; // Get the x position relative to div B
-		const hour = Math.floor(x / hourWidth); // Calculate the hour based on the x position
+		const hour = Math.floor(x / minuteWidth); // Calculate the hour based on the x position
 		const time = addHours(START_TIME, hour); // Calculate the actual time
 		console.log(time);
 	};
 
+	const handleDrag = (e: DraggableEvent, data: DraggableData) => {
+		const { deltaX } = data;
+		const deltaMinutes = deltaX / minuteWidth;
+		setStartTime(addMinutes(startTime, deltaMinutes));
+		setEndTime(addMinutes(endTime, deltaMinutes));
+	};
+
+	const handleResize = (event: SyntheticEvent, data: ResizeCallbackData) => {
+		const { size } = data;
+		const { width } = size;
+		const newEndTime = addMinutes(startTime, width / minuteWidth);
+		setEndTime(newEndTime);
+	};
+
+	const testDrag = () => {
+		console.log("dragging");
+	}
+
 	return (
 		<div id="a" ref={divARef} className="bg-red-200 h-96 w-[80%] overflow-x-auto">
 			<div id="b" ref={divBRef} className={`bg-gradient-to-r from-blue-200 to-black h-32 min-w-full w-[1000px]`} onDoubleClick={handleDoubleClick}>
-
-				<div id="c" style={{
-					left: `${differenceInHours(DIV_C_START_TIME, START_TIME) * hourWidth}px`,
-					width: `${differenceInHours(DIV_C_END_TIME, DIV_C_START_TIME) * hourWidth}px`
-				}} className="relative bg-green-200 h-16">
-				</div>
+				<Draggable onDrag={handleDrag} axis="x" position={{ x: differenceInMinutes(startTime, START_TIME) * minuteWidth, y: 0 }} bounds={"parent"}>
+					{/* <div style={{ width: `${differenceInMinutes(endTime, startTime) * minuteWidth}px` }} className="bg-green-200 h-16"> */}
+					<Resizable width={300} height={100} onResize={handleResize} axis="x">
+						<div id="c" style={{ width: `300px`, height: "100px" }} className="relative flex bg-green-200  h-16 justify-between">
+							<span className="bg-red-500">{format(startTime, "HH:mm")}</span>
+							<span className="bg-red-500">{format(endTime, "HH:mm")}</span>
+						</div>
+					</Resizable>
+					{/* </div> */}
+				</Draggable>
 			</div>
 		</div>
 	)
---
+++ src/components/id/ResizableTimeCardNew.tsx
@@ -0,0 +1,109 @@
+'use client'
+import "@/styles/Components/Resizable.css"
+
+import React, { SyntheticEvent, useState } from "react";
+import Draggable from "react-draggable";
+import { Resizable, ResizeCallbackData } from "react-resizable";
+import Timeline from "@/utils/Timeline";
+import styles from "./TimelineCard.module.scss";
+import { TimeSegment, useSegmentStore } from "@/store/Segments";
+import { differenceInMinutes, format, } from "date-fns";
+
+interface Props {
+	segment: TimeSegment;
+	handleUpdate: (segment: TimeSegment) => void;
+}
+
+export default function ResizableTimeCardNew(props: Props) {
+	const nodeRef = React.useRef(null);
+	const [times, setTimes] = useState({ start: props.segment.start, end: props.segment.end });
+	const [state, setState] = useState({
+		x: Timeline.dateToX(props.segment.start),
+		width: Timeline.minutesToX(differenceInMinutes(props.segment.end, props.segment.start)),
+	});
+
+	const [timeSpan, setTimeSpan] = useState({
+		start: props.segment.start,
+		end: props.segment.end
+	})
+
+	const minWidth = Timeline.minutesToX(30);
+	const bounds = Timeline.getBounds();
+
+	const onResize = (e: SyntheticEvent, { size, handle }: ResizeCallbackData) => {
+		let newX = state.x;
+		let newWidth = size.width;
+		const deltaWidth = state.width - newWidth;
+
+		if (handle === "w") {
+			newX = state.x + deltaWidth
+		}
+		const startDate = Timeline.XToDate(Timeline.snapXToNearestMinutes(newX))
+		const endDate = Timeline.XToDate(Timeline.snapXToNearestMinutes(newX + newWidth))
+		setTimeSpan({ start: startDate, end: endDate })
+
+		setState((currentState) => {
+			if (Timeline.minutesToX(newX) < bounds.min) {
+				return currentState
+			}
+
+			if (size.width < minWidth || newX + newWidth > bounds.max) {
+				return currentState;
+			}
+			return { x: newX, width: newWidth };
+		})
+	};
+
+	const handleUpdate = () => {
+		props.handleUpdate({ id: props.segment.id, start: timeSpan.start, end: timeSpan.end })
+	}
+
+	return (
+		<Draggable
+			axis="x"
+			position={{ x: state.x, y: 0 }}
+			handle=".dragHandle"
+			onDrag={(e, data) => {
+				const newX = Timeline.snapXToNearestMinutes(data.x)
+				const startDate = Timeline.XToDate(newX)
+				const endDate = Timeline.XToDate(newX + state.width)
+				setTimeSpan({ start: startDate, end: endDate })
+			}}
+			onStop={(e, data) => {
+				setState({ ...state, x: Timeline.snapXToNearestMinutes(data.x) })
+				handleUpdate();
+			}}
+			bounds={{ left: bounds.min, right: bounds.max - state.width }}
+			nodeRef={nodeRef}
+		>
+
+			<div
+				ref={nodeRef}
+				className={styles.container}
+				style={{ width: `${state.width}px` }}
+			>
+				<Resizable
+					className={styles.container}
+					width={state.width}
+					height={0}
+					resizeHandles={["e", "w"]}
+					onResize={onResize}
+					onResizeStop={(e, data) => {
+						const newX = Timeline.snapXToNearestMinutes(state.x);
+						const newW = Timeline.snapXToNearestMinutes(state.x + data.size.width) - newX;
+						setState({ ...state, x: newX, width: newW })
+						handleUpdate();
+					}}>
+					<div style={{ width: `${state.width}px` }}>
+						<div className={`dragHandle ${styles.timeContainer} ${"hover:cursor-grab active:cursor-grabbing"}`}>
+							<span className={"p-3 align-center text-ellipsis overflow-hidden font-semibold"}>{format(timeSpan.start, "HH:mm")}</span>
+							<span className={"p-3 align-center text-ellipsis overflow-hidden font-semibold"}>
+								{format(timeSpan.end, "HH:mm")}
+							</span>
+						</div>
+					</div>
+				</Resizable>
+			</div>
+		</Draggable>
+	);
+}
