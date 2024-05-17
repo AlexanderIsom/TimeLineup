4a5ae480458d84bf5f8bd4c50d70203f9585eb8f
Alexander Isom <alexanderdisom@gmail.com>
2024-05-18 00:38:24 +0100

    fix: stop page from redirecting if user is loading

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -10,13 +10,16 @@ import { useEventData, useProfile } from "@/swr/swrFunctions";
 export default function ViewEvent({ params }: { params: { id: string } }) {
 	const { profile, isLoading: profileLoading } = useProfile()
 
-	if (profile === undefined) {
+	if (profile === undefined && !profileLoading) {
 		redirect("/");
 	}
+
 
 	const { eventData, isLoading: isEventLoading } = useEventData(params.id);
 
 	const isMobile = useIsMobile();
+
+	if (!profile) return <></>;
 
 	const localRsvp: EventRsvp = eventData?.rsvps.find(r => r.userId === profile.id) ?? {} as EventRsvp;
 	const otherRsvps: Array<EventRsvp> = eventData?.rsvps.filter(r => r.userId !== profile.id) ?? [];
